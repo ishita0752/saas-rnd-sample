dc0c2e6012b397186dc5fa57fb2109ffe4cfadcd
Ishita <ishitasharma3135@gmail.com>
2026-04-11 17:03:39 +0530

    Prepare for Railway

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from visits.models import PageVisit
 import pathlib
 
-def home_page_view(request,*args,**kwargs):
+def home_view(request,*args,**kwargs):
+    return about_view(request,*args,**kwargs)
+    
+def about_view(request,*args,**kwargs):
     qs=PageVisit.objects.all()
     page_qs=PageVisit.objects.filter(path=request.path)
+    
     my_title="My page"
-    my_conteext={
+    my_context={
         "page_title":my_title,
         "page_visit_count":page_qs.count(),
         "total_visit_count":qs.count(),
@@ -15,4 +19,5 @@ def home_page_view(request,*args,**kwargs):
     path=request.path
     html_template="home.html"
     PageVisit.objects.create(path=request.path)
-    return render(request,html_template,my_conteext)
+    return render(request,html_template,my_context)
+    
