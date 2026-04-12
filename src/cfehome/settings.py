b34be2bc2c9113ed6899e817fe6d63e1c1f945f6
Ishita <ishitasharma3135@gmail.com>
2026-04-12 17:11:04 +0530

    prepare static files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -136,3 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR=BASE_DIR/ "staticfiles"
+STATICFILES_VENDOR_DIR=STATICFILES_BASE_DIR/ "vendors"
+
+STATICFILES_DIRS=[
+    STATICFILES_BASE_DIR
+]
+STATIC_ROOT = BASE_DIR/ "local-cdn"#this is where django's gonna load static files
